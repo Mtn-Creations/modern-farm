b5a0380c3201bbe2c8e34b527a3e553ac2828bfb
Jason Ellison <jason.w.ellison@gmail.com>
2022-07-01 11:22:54 -0400

    Created Tractor module that provides plantSeeds function.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,3 @@
-import { addPlant, usePlants } from "./field.js"
 import { createPlan } from "./plan.js"
 import { createAsparagus } from "./seeds/asparagus.js"
 import { createCorn } from "./seeds/corn.js"
@@ -6,42 +5,46 @@ import { createPotato } from "./seeds/potato.js"
 import { createSoybean } from "./seeds/soybean.js"
 import { createSunflower } from "./seeds/sunflower.js"
 import { createWheat } from "./seeds/wheat.js"
+import { addPlant, usePlants } from "./field.js"
+import { plantSeeds } from "./tractor.js"
 
 
 console.log("Welcome to the main module")
 
-// Create and test plan generation
-const yearlyPlan = createPlan()
+// // Create and test plan generation
+// const yearlyPlan = createPlan()
 
-console.log(yearlyPlan)
+// console.log(yearlyPlan)
 
 
-// Create and test seed generation
-const asparagusSeed = createAsparagus()
-const cornSeed = createCorn()
-const potatoSeed = createPotato()
-const soybeanSeed = createSoybean()
-const sunflowerSeed = createSunflower()
-const wheatSeed = createWheat()
+// // Create and test seed generation
+// const asparagusSeed = createAsparagus()
+// const cornSeed = createCorn()
+// const potatoSeed = createPotato()
+// const soybeanSeed = createSoybean()
+// const sunflowerSeed = createSunflower()
+// const wheatSeed = createWheat()
 
-console.log(asparagusSeed)
-console.log(cornSeed)
-console.log(potatoSeed)
-console.log(soybeanSeed)
-console.log(sunflowerSeed)
-console.log(wheatSeed)
+// console.log(asparagusSeed)
+// console.log(cornSeed)
+// console.log(potatoSeed)
+// console.log(soybeanSeed)
+// console.log(sunflowerSeed)
+// console.log(wheatSeed)
 
-// Populate crop fields with previously generated seeds and test
-addPlant(
-    [
-        asparagusSeed,
-        cornSeed,
-        potatoSeed,
-        soybeanSeed,
-        sunflowerSeed,
-        wheatSeed
-    ]
-)
+// // Populate crop fields with previously generated seeds and test
+// addPlant(
+//     [
+//         asparagusSeed,
+//         cornSeed,
+//         potatoSeed,
+//         soybeanSeed,
+//         sunflowerSeed,
+//         wheatSeed
+//     ]
+// )
 
+const yearlyPlantingPlan = createPlan();
+plantSeeds(yearlyPlantingPlan)
 const field = usePlants()
 console.log(field)
